8fa93fee4b154e0be5f3ae109022e2fb962c4d93
Essa31 <esaaeyad22@gmail.com>
2022-06-22 09:34:17 -0700

    good test

--- tests/test_linked_list_zip.py
+++ tests/test_linked_list_zip.py
@@ -12,29 +12,46 @@ a.append(2)
 b=linkedlist()
 b.append(5)
 b.append(9)
-
+#----------------------------------------------------------
+d=linkedlist()
+d.append(5)
+d.append(9)
+d.append(4)
 
 c=linkedlist()
 c.append(1)
 c.append(3)
+c.append(2)
+#-----------------------------------------------------------
+e=linkedlist()
+
+e.append(1)
+e.append(3)
+
+f=linkedlist()
+f.append(5)
+f.append(9)
+f.append(4)
+
 def test_zip():
+
     a.zipLists(a, b)
 
     actual=a.to_string()
     expected="<1> -- ><5> -- ><3> -- ><9> -- ><2> -- >Null"
-    return actual==expected
+    assert actual==expected
 
 def test_zip2():
-    b.append(4)
-    a.zipLists(a, b)
 
-    actual=a.to_string()
+    c.zipLists(c, d)
+
+    actual=c.to_string()
     expected="<1> -- ><5> -- ><3> -- ><9> -- ><2> -- ><4> -- >Null"
-    return actual==expected
+    assert actual==expected
 
 def test_zip3():
-    a.zipLists(c,b)
+    e.zipLists(e,f)
 
-    actual=a.to_string()
+    actual=e.to_string()
     expected="<1> -- ><5> -- ><3> -- ><9> -- ><4> -- >Null"
-    return actual==expected
+    assert actual==expected
