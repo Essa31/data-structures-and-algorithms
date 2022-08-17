79c13a06676a1ad7131fafb678d605b424f3650b
Essa31 <esaaeyad22@gmail.com>
2022-08-17 09:07:33 -0700

    hashtable

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -67,7 +67,7 @@ class HashTable:
         """
         self.__size = size
         self.__buckets = [None] * size
-        self.__keys_array = []
+        self.__keys = []
 
     def __hash(self, key):
         """
@@ -85,12 +85,12 @@ class HashTable:
             :param value: value of the referenced key
             :return: None
         """
-        hashed_key = self.__hash(key)
-        if self.__buckets[hashed_key] is None:
-            hash_list = LinkedList()
-            self.__buckets[hashed_key] = hash_list
-        self.__keys_array.append(key)
-        self.__buckets[hashed_key].insert((key, value))
+        h_key = self.__hash(key)
+        if self.__buckets[h_key] is None:
+            h_list = LinkedList()
+            self.__buckets[h_key] = h_list
+        self.__keys.append(key)
+        self.__buckets[h_key].insert((key, value))
 
     def get(self, key):
         """
@@ -98,23 +98,23 @@ class HashTable:
             :param key: Hash key
             :return: referenced value by passed key
         """
-        values = []
+        value = []
 
-        hashed_key = self.__hash(key)
-        ll = self.__buckets[hashed_key]
-        if ll is None:
+        h_key = self.__hash(key)
+        linked_list_in_buckets = self.__buckets[h_key]
+        if linked_list_in_buckets is None:
             return None
 
-        current = ll.head
+        current = linked_list_in_buckets.head
         while current:
             if current.value[0] == key:
-                values.append(current.value[1])
+                value.append(current.value[1])
             current = current.next
 
-        if len(values) > 1:
-            return tuple(values)
+        if len(value) > 1:
+            return tuple(value)
         else:
-            return values[0]
+            return value[0]
 
     def contains(self, key):
         """
@@ -133,4 +133,5 @@ class HashTable:
         this method will return a collections of all the keys in hashmap as an object
         :return: an array
         """
-        return self.__keys_array
+        return self.__keys
+
--- tests/test_hashtable.py
+++ tests/test_hashtable.py
@@ -3,14 +3,14 @@ from hashtable.hashtable import *
 
 
 def test_setting_data_into_hash_table(hash_table):
-    hash_table.set("key3", "!!")
+    hash_table.set("key3", "#!!#")
     assert len(hash_table.keys()) == 3
     assert hash_table.keys()[2] == "key3"
 
 
 def test_retrieving_based_on_key(hash_table):
-    assert hash_table.get("key1") == "Hello"
-    assert hash_table.get("key2") == "world"
+    assert hash_table.get("key1") == "Ahmad"
+    assert hash_table.get("key2") == "Ali"
     assert hash_table.contains("key1") is True
 
 
@@ -24,26 +24,26 @@ def test_return_all_keys_in_hash_table(hash_table):
 
 
 def test_handling_collision_within_hash_table(hash_table):
-    hash_table.set("key1", "world")
+    hash_table.set("key1", "Essa")
     assert len(hash_table.get("key1")) == 2
-    hash_table.set("key1", "!!")
+    hash_table.set("key1", "!!@")
     assert len(hash_table.get("key1")) == 3
 
 
 def test_retrieve_value_from_bucket_that_has_collision(hash_table):
-    hash_table.set("key1", "world")
-    assert hash_table.get("key1") == ("world", "Hello")
-    hash_table.set("key1", "!!")
-    assert hash_table.get("key1") == ("!!", "world", "Hello")
+    hash_table.set("key1", "Essa")
+    assert hash_table.get("key1") == ("Essa","Ahmad")
+    hash_table.set("key1", "@")
+    assert hash_table.get("key1") == ("@", "Essa","Ahmad")
 
 
 def test_hash_key_to_in_range_value(hash_table):
-    assert hash_table._HashTable__hash("Hello") == 188
+    assert hash_table._HashTable__hash("Essa") == 452
 
 
 @pytest.fixture
 def hash_table():
     hash_table = HashTable()
-    hash_table.set("key1", "Hello")
-    hash_table.set("key2", "world")
+    hash_table.set("key1", "Ahmad")
+    hash_table.set("key2", "Ali")
     return hash_table
